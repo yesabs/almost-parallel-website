d28d0ce87471f9b7cb4ec58d71ed9d5d4d85b279
yesabs <absk.srma@gmail.com>
2026-03-29 19:01:43 +0200

    Remove old WordPress theme and local node_modules

--- almost-parallel/footer.php
+++
@@ -1,9 +0,0 @@
-</main>
-
-<footer class="ap-footer">
-    <span>&copy; <?php echo date('Y'); ?> Almost Parallel</span>
-</footer>
-
-<?php wp_footer(); ?>
-</body>
-</html>
--- almost-parallel/js/navigation.js
+++
@@ -1,5 +0,0 @@
-// Navigation state machine - stub for Phase 3
-// Will handle the looping click navigation: Home → Theme filter → Color filter → Project/Home
-(function() {
-    'use strict';
-})();
